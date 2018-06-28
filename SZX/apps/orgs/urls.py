0aa008f92111842d2b8a5d4a89f6799c91e788f3
a289237642 <289237642@qq.com>
2018-06-28 14:57:49 +0800

    two

--- SZX/apps/orgs/urls.py
+++ SZX/apps/orgs/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
+from .views import OrgListView
 
 urlpatterns = [
+    url(r'^org_list/$', OrgListView.as_view(), name='org_list')
 ]
--- SZX/apps/orgs/views.py
+++ SZX/apps/orgs/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.views import View
+from orgs.models import OrgInfo
+
 
 # Create your views here.
+class OrgListView(View):
+    def get(self, request):
+        # 拿到所有的数据
+        all_orgs = OrgInfo.objects.all()
+        return render(request, 'orgs/org-list.html', {
+            'all_orgs': all_orgs
+        })
