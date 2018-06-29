044d27e40a7ca41a6d1957c0180a743a5d29d67d
a289237642 <289237642@qq.com>
2018-06-29 21:28:12 +0800

    机构列表页，部分

--- SZX/apps/orgs/views.py
+++ SZX/apps/orgs/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, reverse
 from django.views import View
-from orgs.models import OrgInfo
+from orgs.models import OrgInfo, CityInfo
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 
 # Create your views here.
@@ -8,6 +9,21 @@ class OrgListView(View):
     def get(self, request):
         # 拿到所有的数据
         all_orgs = OrgInfo.objects.all()
+        all_citys = CityInfo.objects.all()
+
+        pagenum = request.GET.get('pagenum', '')
+        pa = Paginator(all_orgs, 3)
+        try:
+            page_list = pa.page(pagenum)
+        except PageNotAnInteger:
+            page_list = pa.page(1)
+        except EmptyPage:
+            page_list = pa.page(pa.num_pages)
+
+        sort_orgs = all_orgs.order_by("-add_time")[:3]
         return render(request, 'orgs/org-list.html', {
-            'all_orgs': all_orgs
+            'all_orgs': all_orgs,
+            'all_citys': all_citys,
+            'sort_orgs': sort_orgs,
+            'page_list': page_list,
         })
